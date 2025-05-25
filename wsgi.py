da3005ef09a29dcbc8ab57b63b901a3280f4fa94
Miguel <mjosalvarez@poligran.edu.co>
2025-05-25 14:08:39 -0500

    Actualiza configuración de wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
 from app import create_app
 
+# Cargar variables de entorno
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+# Crear la aplicación
 app = create_app()
 
 if __name__ == "__main__":
-    app.run() 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) 
