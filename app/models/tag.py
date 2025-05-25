4a804bc2bd375591df3644f5792d194e90c38948
Miguel <mjosalvarez@poligran.edu.co>
2025-05-25 14:39:15 -0500

    Agregado extend_existing=True a los modelos para evitar conflictos de tabla

--- app/models/tag.py
+++ app/models/tag.py
@@ -3,17 +3,20 @@ from app import db
 # Tabla de asociación entre tags y tareas
 task_tags = db.Table('task_tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-    db.Column('task_id', db.Integer, db.ForeignKey('task.id'), primary_key=True)
+    db.Column('task_id', db.Integer, db.ForeignKey('task.id'), primary_key=True),
+    extend_existing=True
 )
 
 # Tabla de asociación entre tags y proyectos
 project_tags = db.Table('project_tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-    db.Column('project_id', db.Integer, db.ForeignKey('project.id'), primary_key=True)
+    db.Column('project_id', db.Integer, db.ForeignKey('project.id'), primary_key=True),
+    extend_existing=True
 )
 
 class Tag(db.Model):
     __tablename__ = 'tag'
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
